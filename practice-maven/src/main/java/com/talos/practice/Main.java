d713853ae8246576a24ca3cef4b9aab741f5e28d
talos <17558819220@163.com>
2023-04-24 20:14:21 +0800

    Initial commit

---
+++ practice-maven/src/main/java/com/talos/practice/Main.java
@@ -0,0 +1,14 @@
+package com.talos.practice;
+
+/**
+ * TODO
+ *
+ * @Description
+ * @Author ${USER}
+ * @Date ${DATE} ${TIME}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ practice-spring/src/test/java/com/talos/practice/practicespring/PracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talos.practice.practicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
